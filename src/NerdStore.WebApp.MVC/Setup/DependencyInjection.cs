3695c32c90fbc1ff42e5d255ea0bc6e3b2b58418
Farllon <farllon16@outlook.com>
2021-02-02 18:54:42 -0300

    Adicionando migrações

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
+using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Core.Bus;
 
@@ -14,7 +15,7 @@ namespace NerdStore.WebApp.MVC.Setup
             services.AddScoped<IMediatrHandler, MediatrHandler>();
 
             // Catalogo
-            services.AddScoped<IProdutoRepository, IProdutoRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IProdutoAppService, ProdutoAppService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
